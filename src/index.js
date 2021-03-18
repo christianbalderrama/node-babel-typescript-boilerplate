7c6005b35888c46e4da7f139fd322e59c05262a1
Christian Balderrama <0christianbalderrama@gmail.com>
2021-03-19 00:19:53 +0800

    :sparkes: Setup eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from "express";
-import morgan from "morgan";
+import express from 'express';
+import morgan from 'morgan';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan("dev"));
-app.get("/", (_, res) => {
-  return res.send("Hello, World!");
+app.use(morgan('dev'));
+app.get('/', (_, res) => {
+  return res.send('Hello, World!');
 });
 
 app.listen(port, () => console.log(`Server is listening to port: ${port}`));
